b51715c5936221d29dd8ce34bee69c1c6a19c4fe
DevlopAmine <aminehouri47@gmail.com>
2017-05-03 18:01:18 +0100

    3/05/17

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,28 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
+import { RouterModule }   from '@angular/router';
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroService }         from './hero.service';
+
+import { AppRoutingModule }     from './app-routing.module';
+
 
 
 @NgModule({
   imports:      
-  [ BrowserModule,FormsModule
+  [ BrowserModule,FormsModule,AppRoutingModule
    ],
-  declarations: [ AppComponent ,HeroDetailComponent],
+   providers: [
+    HeroService
+  ],
+  declarations: [ AppComponent ,HeroDetailComponent,HeroesComponent,DashboardComponent],
   bootstrap:    [ AppComponent ]
 })
+
+
+
 export class AppModule { }
